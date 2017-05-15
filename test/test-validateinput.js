9c5fa74bd65075347f98d747b9d60cdd774b5b37
Gregor Armann <gregor.armann@googlemail.com>
2017-05-15 23:17:52 +0200

    removed old code from tests

--- test/test-validateinput.js
+++ test/test-validateinput.js
@@ -77,28 +77,4 @@ describe('check ValidateInput from lib/validateinput.js', function(){
     ValidateInput.checkInputUserData(user2).should.be.false;
   });
 
-
-
 });
-
-/*
-describe('checks checkShootingExists from api-functions', function(){
-  
-  it('should check if shooting with id exists (1-7)', function(){
-    for (var i = 1;i<=7; i++){
-      api.checkShootingExists(i).should.be.true;
-    }
-  });
-  it('should be false with no parameter', function(){
-    api.checkShootingExists().should.be.false;
-  });
-  it('should be false with a string as parameter', function(){
-    api.checkShootingExists('string').should.be.false;
-  });
-});
-describe('checks nextFreeId from api-functions', function(){
-  it('should be 8 with current testing data', function(){
-    api.nextFreeId().should.be.equal(8);
-  });
-});
-*/
